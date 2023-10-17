2abfdf1fb6c73063515166988ce7722530cb756f
paolo <paolo.penazzi@studio.unibo.it>
2023-10-17 18:02:15 +0200

    ci: add semantic release

---
+++ release.config.js
@@ -0,0 +1,17 @@
+const config = require('semantic-release-preconfigured-conventional-commits')
+
+const releaseBranches = ["main"]
+config.branches = releaseBranches
+config.plugins.push(
+    ["@semantic-release/exec", {
+    }],
+    ["@semantic-release/github", {
+        "assets": [
+        ]
+    }],
+    ["@semantic-release/git", {
+        "assets": ["CHANGELOG.md", "package.json"],
+        "message": "chore(release)!: [skip ci] ${nextRelease.version} released"
+    }],
+)
+module.exports = config
