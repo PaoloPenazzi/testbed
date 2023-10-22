0663beee8315bf24bf281d820d49a2e994bff644
paolo <paolo.penazzi@studio.unibo.it>
2023-10-22 17:47:20 +0200

    refactor: remove unused methods and add serializable tag

--- src/main/kotlin/model/Benchmark.kt
+++ src/main/kotlin/model/Benchmark.kt
@@ -1,16 +1,11 @@
 package model
 
+import kotlinx.serialization.Serializable
+
 interface Benchmark {
     val simulator: Simulator
     val input: String
-    fun withSimulator(simulator: Simulator): Benchmark
-    fun withInput(input: String): Benchmark
 }
 
-data class BenchmarkImpl(override val simulator: Simulator = SimulatorImpl(),
-                         override val input: String = ""): Benchmark {
-
-    override fun withSimulator(simulator: Simulator): Benchmark = copy(simulator = simulator)
-
-    override fun withInput(input: String): Benchmark = copy(input = input)
-}
+@Serializable
+data class BenchmarkImpl(override val simulator: SimulatorImpl, override val input: String): Benchmark
--- src/main/kotlin/model/Simulator.kt
+++ src/main/kotlin/model/Simulator.kt
@@ -1,15 +1,11 @@
 package model
 
+import kotlinx.serialization.Serializable
+
 interface Simulator {
     val name: String
     val version: String
-    fun withName(name: String): Simulator
-    fun withVersion(version: String): Simulator
 }
 
-data class SimulatorImpl(override val name: String = "", override val version: String = ""): Simulator {
-
-    override fun withName(name: String): Simulator = copy(name = name)
-
-    override fun withVersion(version: String): Simulator = copy(version = version)
-}
+@Serializable
+data class SimulatorImpl(override val name: String, override val version: String): Simulator
