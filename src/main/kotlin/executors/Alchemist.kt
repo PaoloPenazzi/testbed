b0ad0a3eccb00a63f03ae75157e84fe0f342cabb
paolo <paolo.penazzi@studio.unibo.it>
2023-10-24 15:09:11 +0200

    feat: update testbed model

--- src/main/kotlin/executors/Alchemist.kt
+++ src/main/kotlin/executors/Alchemist.kt
@@ -3,7 +3,6 @@ package executors
 import it.unibo.alchemist.Alchemist
 
 class AlchemistDriver {
-    // a public method to call the Alchemist main method
     fun runAlchemist() {
         println("starting Alchemist...")
         Alchemist.main(arrayOf("run", "./src/main/yaml/simulation.yml"))
