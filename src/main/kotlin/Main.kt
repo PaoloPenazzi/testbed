50b85f91d27581026e4f65ea02726416fda5bf8c
paolo <paolo.penazzi@studio.unibo.it>
2023-10-29 16:06:12 +0100

    feat: enable the execution of more program in the same scenario

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,5 @@ import controller.ControllerImpl
 fun main(args: Array<String>) {
     // Run the controller
     val controller = ControllerImpl()
-    controller.run("./src/main/yaml/testbed-1scen-2prog.yml")
+    controller.run("./src/main/yaml/benchmarks/multiple-programs.yml")
 }
--- src/main/kotlin/executors/Alchemist.kt
+++ src/main/kotlin/executors/Alchemist.kt
@@ -6,6 +6,6 @@ class Alchemist: SimulatorExecutor {
     override fun run(input: String) {
         println("starting Alchemist...")
         println("Alchemist path $input")
-        // Alchemist.main(arrayOf("run", input))
+        Alchemist.main(arrayOf("run", input))
     }
 }
