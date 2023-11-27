6b0b4b231035978e2cdfd56fb921481ab7c1ab70
Paolo Penazzi <58599380+PaoloPenazzi@users.noreply.github.com>
2023-11-27 14:28:43 +0100

    ci!: fix release

--- release.config.js
+++ release.config.js
@@ -1,9 +1,15 @@
 const config = require('semantic-release-preconfigured-conventional-commits')
 
+const publishCommands = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
+git push --force origin \${nextRelease.version} || exit 2
+`
+
 const releaseBranches = ["main"]
 config.branches = releaseBranches
 config.plugins.push(
     ["@semantic-release/exec", {
+        "publishCmd": publishCommands
     }],
     ["@semantic-release/github", {
         "assets": [
