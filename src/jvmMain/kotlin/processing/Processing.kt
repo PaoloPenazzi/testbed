78e36cb9a7792d5834fe942d62de48e435251f54
paolo <paolo.penazzi@studio.unibo.it>
2024-01-11 12:07:05 +0100

    feat: provide a post-processing example. add sequence diagrams

--- src/jvmMain/kotlin/processing/Processing.kt
+++ src/jvmMain/kotlin/processing/Processing.kt
@@ -4,25 +4,33 @@ import model.BenchmarkOutput
 import model.Output
 import model.Result
 import model.VisualisationType
+import java.math.RoundingMode
 
 val process: (BenchmarkOutput) -> Output = { benchmarkOutput ->
-    // NetLogo processing
-    val netlogoMetric = benchmarkOutput["NetLogo-Tutorial-1"]
-    val wolvesCounter = netlogoMetric?.get("count wolves")
-    val wolvesAverage = wolvesCounter.orEmpty().map {
-        when (it) {
-            is Int -> it
-            is String -> it.toInt()
-            else -> throw IllegalArgumentException("Unexpected type")
-        }
-    }.average()
-    val wolfResult = Result("Average number of wolves: ", listOf(wolvesAverage), VisualisationType.SINGLE_VALUE)
+    val averageNumberOfWolvesPerRun = averageNumberOfWolvesPerRun(benchmarkOutput)
 
     // Alchemist processing
     val alchemistMetric = benchmarkOutput["Alchemist-Protelis-1"]
     val timeCounter = alchemistMetric?.get("time ")
-    val timeResult = Result("Time steps done: ", timeCounter.orEmpty(), VisualisationType.LIST)
+    val timeResult = Result("Alchemist time steps: ", timeCounter.orEmpty(), VisualisationType.LIST_OF_VALUES)
 
     // Return the list of results
-    listOf(wolfResult, timeResult)
+    listOf(averageNumberOfWolvesPerRun, timeResult)
+}
+
+fun averageNumberOfWolvesPerRun(benchmarkOutput: BenchmarkOutput): Result {
+    val netlogoRunsNumber = 3
+    var wolvesAverageList = listOf<Double>()
+    for (i in 1..netlogoRunsNumber) {
+        val wolvesCounter = benchmarkOutput["NetLogo-Tutorial-$i"]?.get("count wolves")
+        val wolvesAverage = wolvesCounter.orEmpty().map {
+            when (it) {
+                is Int -> it
+                is String -> it.toInt()
+                else -> throw IllegalArgumentException("Unexpected type")
+            }
+        }.average()
+        wolvesAverageList = wolvesAverageList + wolvesAverage.toBigDecimal().setScale(2, RoundingMode.HALF_UP).toDouble()
+    }
+    return Result("Average number of wolves per run: ", wolvesAverageList, VisualisationType.LIST_OF_VALUES)
 }
