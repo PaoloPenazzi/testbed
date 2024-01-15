02624b0c1a4f7a1569f702a1133a4c1b9e6fd7f0
Paolo Penazzi <58599380+PaoloPenazzi@users.noreply.github.com>
2024-01-15 16:26:44 +0100

    ci: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,8 +59,8 @@ publishOnCentral {
     projectLongName.set("Testbed")
     projectDescription.set("An open benchmarking platform for Collective Adaptive Systems")
     repository("https://maven.pkg.github.com/paolopenazzi/${rootProject.name}".lowercase()) {
-        user.set(System.getenv("MAVEN_CENTRAL_USERNAME"))
-        password.set(System.getenv("MAVEN_CENTRAL_PASSWORD"))
+        user.set("paolopenazzi")
+        password.set(System.getenv("GH_TOKEN"))
     }
     publishing {
         publications {
