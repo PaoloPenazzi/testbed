8d26647721093d731c471db029612df7b018b579
paolo <paolo.penazzi@studio.unibo.it>
2024-01-15 12:07:26 +0100

    refactor: change class name to better reflect their purpose

--- src/main/kotlin/listeners/NetLogoListener.kt
+++ src/main/kotlin/listeners/NetLogoListener.kt
@@ -4,7 +4,7 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Paths
 
-class NetLogoListenerImpl : Listener {
+class NetLogoListener : Listener {
     override fun clearCSV(outputFilePath: String) {
         var lines = Files.readAllLines(Paths.get(outputFilePath), StandardCharsets.UTF_8)
         repeat(6) {
