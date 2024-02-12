b2a9788c712432875bddfd82ab293d464e51c89d
Paolo Penazzi <paolo.penazzi@studio.unibo.it>
2024-02-12 10:35:27 +0100

    fix: add broken files

--- src/main/kotlin/executors/NetLogoExecutor.kt
+++ src/main/kotlin/executors/NetLogoExecutor.kt
@@ -5,6 +5,13 @@ import java.io.File
 
 /**
  * An executor for NetLogo.
+<<<<<<< HEAD
+=======
+ * Creates the following command:
+ * ./NetLogo_Console --headless --model <model-file> --setup-file <setup-file> --table ./export.csv
+ * where <model-file> is the second input file and <setup-file> is the first input file.
+ * This command will be called from the folder where the simulator is located.
+>>>>>>> b71c7d5 (fix: add broken files)
  */
 class NetLogoExecutor : Executor {
     override fun getCommand(simulatorPath: String, scenario: Scenario): ProcessBuilder {
@@ -12,9 +19,15 @@ class NetLogoExecutor : Executor {
             "./NetLogo_Console",
             "--headless",
             "--model",
+<<<<<<< HEAD
             scenario.modelPath,
             "--setup-file",
             scenario.input,
+=======
+            scenario.input[1],
+            "--setup-file",
+            scenario.input[0],
+>>>>>>> b71c7d5 (fix: add broken files)
             "--table",
             "./export.csv",
         )
